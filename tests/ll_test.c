4abc21d4811170bb08105e568a3134930adaf046
Matthias Beyer <matthias@ib-fb.de>
2012-10-06 21:47:07 +0200

    use new test_exists function, added stack tests

--- tests/ll_test.c
+++ tests/ll_test.c
@@ -438,7 +438,7 @@ static void cleanup(LinkedList *l) {
 int main(void) {
     int i;
     bool worked = true;
-    for( i = 0; tests[i].desc && worked; i++ ) {
+    for( i = 0; test_exists(tests[i]) && worked; i++ ) {
         worked = test_exec( &tests[i] );
     }
     return worked;
--- tests/stack_test.c
+++ tests/stack_test.c
@@ -1,22 +1,72 @@
 #include "stack/stack.h"
 #include "utilc_test_utils.h"
 
-int main(void) {
-    Stack *stack = empty_stack();
-    int test_ints[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
-    //double test_doubles[] = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 0.0 };
+static bool test_pushing(void);
+static bool test_poping(void);
 
-    unsigned int i;
-    for( i = 0 ; i < len(test_ints); i++ ) {
-        stackpush( stack, sizeof( test_ints[i] ), &test_ints[i] );
-    }
-    
+static void cleanup(Stack *stack);
+static void cleanup_element(StackElement *ste);
+
+/* desc, strict, bool(*testfunc)(), result*/
+Test tests[] = {
+    {"Pushing", true, test_pushing, false },
+    {"Poping",  true, test_poping,  false },
+    {NULL, NULL, NULL, NULL}
+}
+
+static bool test_pushing() {
+    Stack *stack = empty_stack();
+    bool result = false;
+    int topush = 1;
+
+    stackpush(stack, sizeof(topush), &topush);
+    result =    (stack->first->next == NULL) && 
+                (stack->first->data_size == sizeof(topush)) &&
+                (stack->first->data == topush);
+
+    cleanup(stack);
+    return result;
+}
+
+static bool test_poping() {
+    bool d = depends(test_pushing);
+    if (!d) return false;
+
+    Stack *stack = empty_stack();
+    int ary[] = { 1, 2, 3, 4, 5 };
     bool worked = true;
-    while( i > 0 && worked ) {
-        worked = test_ints[i] == *((int*)stackpop(stack));
-    }
-    if(worked)
-        printf("Worked!\n");
+
+    int i;
+    for( i = 0; i<len(ary); i++ )
+        stackpush(stack, sizeof(ary[i]), &ary[i] );
+
+    for( i = 0 ; i<len(ary); i++ )
+        worked = ary[i] == (int)stackpop(stack);
+
+    return worked;
+}
+
+/*
+ * Cleanup
+ */
+static void cleanup(Stack *stack) {
+    cleanup_element(stack->first);
+    free(stack);
+}
+
+static void cleanup_element(StackElement *ste) {
+    if( ste->next )
+        cleanup(ste->next);
     else
-        printf("Does not work!\n");
+        free(ste);
+}
+
+int main(void) {
+    unsigned int i;
+    bool worked = true;
+    for( i = 0; test_exists(tests[i]) && worked; i++ ) {
+        worked = test_exec(tests[i]);
+    }
+
+    return worked;
 }
