049ec781c59ff41b472bf49f380eb0aca2afd06f
Matthias Beyer <matthias@ib-fb.de>
2012-10-06 13:32:50 +0200

    debug output added, small fixes

--- src/stack/stack.c
+++ src/stack/stack.c
@@ -1,14 +1,26 @@
 #include "stack/stack.h"
 
+#ifdef DEBUG
+#include <stdio.h>
+#endif
+
 static StackElement * stackelement( size_t data_size, void *data );
 
 Stack * empty_stack() {
+#ifdef DEBUG
+    printf("Create Stack\n");
+#endif // DEBUG
+
     Stack *stack = (Stack*) malloc( sizeof(Stack) );
     stack->first = NULL;
     return stack;
 }
 
 static StackElement * stackelement( size_t data_size, void *data ) {
+#ifdef DEBUG
+    printf("Create StackElement\n");
+#endif // DEBUG
+
     StackElement *element = (StackElement*) malloc( sizeof(StackElement) + data_size );
     element->next = NULL;
     element->data_size = data_size;
@@ -17,12 +29,20 @@ static StackElement * stackelement( size_t data_size, void *data ) {
 }
 
 void stackpush( Stack *stack, size_t data_size, void *data ) {
+#ifdef DEBUG
+    printf("Stack: Push\n");
+#endif // DEBUG
+
     StackElement *element = stackelement(data_size, data);
     element->next = stack->first;
     stack->first = element;
 }
 
 void * stackpop( Stack *stack ) {
+#ifdef DEBUG
+    printf("Stack: Pop\n");
+#endif // DEBUG
+
     void *ret = malloc( stack->first->data_size );
     memcpy( ret, &stack->first->data, stack->first->data_size );
     StackElement *next = stack->first->next;
--- tests/stack_test.c
+++ tests/stack_test.c
@@ -1,20 +1,22 @@
 #include "stack/stack.h"
+#include <stdbool.h>
+#include <stdio.h>
 
 #define len(x) (sizeof(x)/sizeof(x[0]))
 
 int main(void) {
     Stack *stack = empty_stack();
     int test_ints[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
-    double test_doubles[] = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 0.0 };
+    //double test_doubles[] = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 0.0 };
 
     int i;
     for( i = 0 ; i < len(test_ints); i++ ) {
-        stackpush( stack, sizeof( test_ints[i] ); &test_ints[i] );
+        stackpush( stack, sizeof( test_ints[i] ), &test_ints[i] );
     }
     
     bool worked = true;
     while( i > 0 && worked ) {
-        worked = test_ints[i] == stackpop(stack);    
+        worked = test_ints[i] == *((int*)stackpop(stack));
     }
     if(worked)
         printf("Worked!\n");
