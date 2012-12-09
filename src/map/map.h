1fd7b34fbaf1054eeb4993e3e5fb51e4ab3dba2e
Matthias Beyer <matthias@ib-fb.de>
2012-12-09 18:22:47 +0100

    added stuff
    - includes
    - variables in MapElement-type
    - some prototypes (embelishments)

--- src/map/map.h
+++ src/map/map.h
@@ -1,6 +1,9 @@
 #ifndef __UTILC_MAP_H__
 #define __UTILC_MAP_H__
 
+#include <stdlib.h>
+#include <string.h>
+
 typedef struct {
     size_t value_size;
     void * key;
@@ -9,6 +12,7 @@ typedef struct {
 
 typedef struct {
     unsigned int len;
+    unsigned int cnt_set_elements;
     signed int (* cmpfunc)( void * key_x, void * key_y );
 
     MapElement elements[];
@@ -18,15 +22,20 @@ typedef enum {
     MAP_OK,
     MAP_UNABLE_TO_ALLOC,
     MAP_KEY_EXISTS,
-    MAP_OTHER_ERROR,
+    MAP_UNKNOWN_ERROR,
 
     MAP_STATUS_LEN,
 } MapInsertStatus;
 
-Map *               map_new_map(void);
+Map *               map_new_map(unsigned int prealloc, 
+                                signed int (*cmpfunc)(void* key_x, void* key_y));
 void                map_delete_map(Map *m);
 
-MapInsertStatus     map_insert(void *key, void *value);
+MapInsertStatus     map_insert( Map *m, 
+                                void *key, 
+                                size_t keysize,
+                                void *value,
+                                size_t valuesize);
 void *              map_find(void *key);
 void *              map_delete_by_key(void *key);
 void *              map_delete_by_value(void *val);
